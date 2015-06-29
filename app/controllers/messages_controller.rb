89a0b84c924c62105a1b265f91529c00b0058269
HansWurst90 <Jan@mussenbrock-w.de>
2015-06-29 18:23:46 +0200

    Timeout für Message-Get hinzugefügt

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,47 +6,50 @@ class MessagesController < ApplicationController
     @outerMessage = OuterMessage.new(outer_message_params)
     @outerMessage.recipient = params[:username]
     if (Time.now.to_i-@outerMessage.timestamp) > 300
-      render json: {"status"=> "1", "error"=> "Request is older than 5 minutes"}, status: 1
+     render json: { "Status" => "408 Request Time-out"}, status: :request_timeout
     else
       # @pubkey = User.find(@outerMessage.sender).pubkey_user
       # @signature = @outerMessage.sig_service
       # digest = OpenSSL::Digest::SHA256.new
       # @data = @outerMessage.sender + @outerMessage.cipher + @outerMessage.iv + @outerMessage.key_recipient_enc + @outerMessage.sig_recipient + @outerMessage.timestamp + params[:id_recipient]
       # if @pubkey.verify digest, @signature, @data
+      # else
+      #   render json: { "Status" => "401 Unauthorized"}, status: :unauthorized
       
-        @message = Message.new( :recipient => params[:username],
+      @message = Message.new( :recipient => params[:username],
                                 :sender => @outerMessage.sender,
                                 :cipher => @outerMessage.cipher,
                                 :iv => @outerMessage.iv,
                                 :key_recipient_enc => @outerMessage.key_recipient_enc,
                                 :sig_recipient => @outerMessage.sig_recipient,)
-          if @message.save 
-            render json: @message, :only => [:username ,:sender, :cipher, :iv, :key_recipient_enc, :sig_recipient], status: :created
-          else
-            render json: @message.errors, status: :unprocessable_entity
+        if @message.save 
+          render json: @message, :only => [:username ,:sender, :cipher, :iv, :key_recipient_enc, :sig_recipient], status: :created
+        else
+          render json: @message.errors, status: :unprocessable_entity
         end
-      # else
-      #   render json: {"status"=> "2", "error"=> "Signature not valid"}, status: 2
-      # end
+      end
     end
-  end
 
   def recieve
-    # Wie werden Parameter per GET mitgeschickt?
-
-    # if (Time.now.to_i-timestamp) > 300
-    #  render json: {"status"=> "1", "error"=> "Request is older than 5 minutes"}, status: 1
-    # else
-
+    # Krypto-Vorbereitung
     # digest = OpenSSL::Digest::SHA256.new
     # @pubkey = User.find(username).pubkey_user
-    # @signature = signatur des Abfragenden
-    # if @pubkey.verify digest, @signature, Identität+Timestamp
-    @messages = Message.where(:recipient => params[:username])
-    render json: @messages, :only => [:sender, :cipher, :iv, :key_recipient_enc, :sig_recipient]
-    # else
-    #   render json: {"status"=> "2", "error"=> "Signature not valid"}, status: 2
-    # end
+
+    # Empfang der Parameter
+    @timestamp = params[:timestamp].to_f
+    @signature = params[:signature]
+    # Time-out-Abfrage
+    if (Time.now.to_i-@timestamp) > 300
+     render json: { "Status" => "408 Request Time-out"}, status: :request_timeout
+    else
+      # if @pubkey.verify digest, @signature, Identität+Timestamp
+        # Liefern der Nachricht
+        @messages = Message.where(:recipient => params[:username])
+        render json: @messages, :only => [:sender, :cipher, :iv, :key_recipient_enc, :sig_recipient]
+      # else
+      #   render json: { "Status" => "401 Unauthorized"}, status: :unauthorized
+      # end
+    end
   end
 
 
