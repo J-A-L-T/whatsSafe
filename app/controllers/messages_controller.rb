7c485e242e385e4952cce3548af8547b9f3efea0
HansWurst90 <Jan@mussenbrock-w.de>
2015-06-29 21:26:38 +0200

    Methode zum Testen und verstehen des Signaturverifizierens hinzugefügt. Webservice sollte jetzt (bis auf die Signaturverifizierung) laufen.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,10 @@
 require 'openssl' # Dokumentation dazu: http://ruby-doc.org/stdlib-2.0/libdoc/openssl/rdoc/OpenSSL.html
+require 'Base64'  # Dokumentation dazu: http://api.rubyonrails.org/v3.2.0/classes/Base64.html
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :update, :destroy]
 
   def create
+    # => Empfänger überhaupt vorhanden?
     if !User.exists?(username: params[:username])
       render json: { "Status" => "404 Not Found"}, status: :not_found
       logger.info "################# LOG #################"
@@ -11,43 +13,48 @@ class MessagesController < ApplicationController
     else 
       @outerMessage = OuterMessage.new(outer_message_params)
       @outerMessage.recipient = params[:username]
+      # => Nachrichtenalter entsprich noch jünger als 5 min?
       if (Time.now.to_i-@outerMessage.timestamp) > 300
        render json: { "Status" => "408 Request Time-out"}, status: :request_timeout
        logger.info "################# LOG #################"
        logger.info "Status: 408 - Request Time-out"
        logger.info "################# LOG #################"
       else
+        # ----- WICHITG BASE64 DECODE - "aus als String empfangenen Kryptosachen wieder echte machen" => Base64.decode64("String") ----- #
+
+        # => Krypto-Vorbereitung
         # @pubkey = User.find_by_username(@outerMessage.sender).pubkey_user
         # @signature = @outerMessage.sig_service
         # digest = OpenSSL::Digest::SHA256.new
         # @data = @outerMessage.sender + @outerMessage.cipher + @outerMessage.iv + @outerMessage.key_recipient_enc + @outerMessage.sig_recipient + @outerMessage.timestamp + params[:username]
+        # => Signatur ist verifiziert? => http://ruby-doc.org/stdlib-2.0/libdoc/openssl/rdoc/OpenSSL.html
         # if @pubkey.verify digest, @signature, @data
-        # else
-        #   render json: { "Status" => "401 Unauthorized"}, status: :unauthorized
-            # logger.info "################# LOG #################"
-            # logger.info "Status: 401 - Unauthorized"
-            # logger.info "################# LOG #################"
-        
         @message = Message.new( :recipient => params[:username],
                                 :sender => @outerMessage.sender,
                                 :cipher => @outerMessage.cipher,
                                 :iv => @outerMessage.iv,
                                 :key_recipient_enc => @outerMessage.key_recipient_enc,
                                 :sig_recipient => @outerMessage.sig_recipient,)
-          if @message.save 
-            render json: @message, :only => [:username ,:sender, :cipher, :iv, :key_recipient_enc, :sig_recipient], status: :created
-            logger.info "################# LOG #################"
-            logger.info "Status: 201 - Created "
-            logger.info "################# LOG #################"
-          else
-            render json: @message.errors, status: :unprocessable_entity
-            logger.info "################# LOG #################"
-            logger.info "Status: 422  - Unprocessable Entity"
-            logger.info "################# LOG #################"
-          end
+        # else
+          # render json: { "Status" => "401 Unauthorized"}, status: :unauthorized
+          # logger.info "################# LOG #################"
+          # logger.info "Status: 401 - Unauthorized"
+          # logger.info "################# LOG #################"
+        # => Nachricht wurde gespeichert?
+        if @message.save 
+          render json: @message, :only => [:username ,:sender, :cipher, :iv, :key_recipient_enc, :sig_recipient], status: :created
+          logger.info "################# LOG #################"
+          logger.info "Status: 201 - Created "
+          logger.info "################# LOG #################"
+        else
+          render json: @message.errors, status: :unprocessable_entity
+          logger.info "################# LOG #################"
+          logger.info "Status: 422  - Unprocessable Entity"
+          logger.info "################# LOG #################"
         end
       end
     end
+  end
 
   def recieve
     if !User.exists?(username: params[:username])
@@ -56,20 +63,22 @@ class MessagesController < ApplicationController
       logger.info "Status: 404 - Not Found"
       logger.info "################# LOG #################"
     else
-      # Krypto-Vorbereitung
+      # ----- WICHITG BASE64 DECODE - "aus als String empfangenen Kryptosachen wieder echte machen" => Base64.decode64("String") ----- #
+      # => Krypto-Vorbereitung
       # digest = OpenSSL::Digest::SHA256.new
       # @pubkey = User.find(username).pubkey_user
 
-      # Empfang der Parameter
+      # => Empfang der Parameter
       @timestamp = params[:timestamp].to_f
       @signature = params[:signature]
-      # Time-out-Abfrage
+      # => Abfragealter entsprich noch jünger als 5 min?
       if (Time.now.to_i-@timestamp) > 300
        render json: { "Status" => "408 Request Time-out"}, status: :request_timeout
        logger.info "################# LOG #################"
        logger.info "Status: 408 - Request Time-out"
        logger.info "################# LOG #################"
       else
+        # => Signatur ist verifiziert? => http://ruby-doc.org/stdlib-2.0/libdoc/openssl/rdoc/OpenSSL.html
         # if @pubkey.verify digest, @signature, Identität+Timestamp
           # Liefern der Nachricht
           @messages = Message.where(:recipient => params[:username])
@@ -87,6 +96,26 @@ class MessagesController < ApplicationController
     end
   end
 
+  def signatur
+    # => Krypto-Vorbereitung - erstellen eines Schlüsselpaars
+    key = OpenSSL::PKey::RSA.new(2048)
+    private_key = key
+    public_key = key.public_key
+
+    # => Signatur erstellen 
+    digest1 = OpenSSL::Digest::SHA256.new
+    document = "Test" + "noch ein Test"
+    signature = private_key.sign digest1, document
+
+    # => Signatur prüfen 
+    digest2 = OpenSSL::Digest::SHA256.new
+    document = "Test" + "noch ein Test"
+    if public_key.verify digest2, signature, document
+      render json: { "Signature" => "valid"}
+    else
+      render json: { "Signature" => "not valid"}
+    end
+  end
 
   private
     def set_message
