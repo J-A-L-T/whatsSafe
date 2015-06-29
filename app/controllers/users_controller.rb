98a9525d0f052f66c6ffb77e8150c84f6efc1976
HansWurst90 <Jan@mussenbrock-w.de>
2015-06-29 12:10:28 +0200

    Nur einigartige Benutzernamen sind zugelassen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,14 @@ end
 
   def create
     @user = User.new(user_params)
-
-    if @user.save 
-      render json: @user, :only => [:username], status: :created
+    if User.exists?(username: @user.username)
+      render json: { "Status" => "409 Conflict"}, status: :conflict
     else
-      render json: @user.errors, status: :unprocessable_entity
+      if @user.save 
+        render json: @user, :only => [:username, :id], status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
   end
 
